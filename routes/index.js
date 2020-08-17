cd9f03793897ec31b1761019b7ced6630b041541
hannah-developer <hannahchoi.developer@gmail.com>
2020-08-17 13:09:55 +0900

    created /create route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../bin/db.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/create',function(req,res){
+  res.render('create')
+})
+
+router.post('/create',function(req,res){
+  connection.query(`insert into bookquotes(author,title,contents) values('${req.body.author}','${req.body.booktitle}','${req.body.book_contents}')`, function(err, rows, fields) {
+    if (err) throw err;
+    res.redirect('/')
+  });
+  
+})
 
 module.exports = router;
