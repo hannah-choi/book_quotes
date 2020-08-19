9d66867acca49747c0a7eb098cb9d7561f6a7099
hannah-developer <hannahchoi.developer@gmail.com>
2020-08-19 15:07:37 +0900

    modified router.post('/create')

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,21 @@ router.get('/create',function(req,res){
 })
 
 router.post('/create',function(req,res){
-  if(req.body.author && req.body.booktitle && req.body.book_contents){
   connection.query(`insert into bookquotes(author,title,contents) values('${req.body.author}','${req.body.booktitle}','${req.body.book_contents}')`, function(err, rows, fields) {
     if (err) throw err;
     res.redirect('/')
-  });}
-  else { //db에 들어가는 것 막기
-    res.redirect('/create')
-  }
+  });
 })
+
+// router.post('/create',function(req,res){
+//   if(req.body.author && req.body.booktitle && req.body.book_contents){
+//   connection.query(`insert into bookquotes(author,title,contents) values('${req.body.author}','${req.body.booktitle}','${req.body.book_contents}')`, function(err, rows, fields) {
+//     if (err) throw err;
+//     res.redirect('/')
+//   });}
+//   else { //db d
+//     res.redirect('/create')
+//   }
+// })
 
 module.exports = router;
