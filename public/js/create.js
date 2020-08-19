d9d2a06eccc89b5c903062b0da00c4078bc08d0f
hannah-developer <hannahchoi.developer@gmail.com>
2020-08-19 19:09:50 +0900

    moved variables into addEventListener

--- public/js/create.js
+++ public/js/create.js
@@ -12,8 +12,12 @@ const form = document.querySelector('form')
 // }
 
 form.addEventListener('submit', (e)=>{
-    console.log(form.booktitle.value,form.author.value,form.book_contents.value)
-    if(form.booktitle.value=="" || form.author.value=="" || form.book_contents.value==""){ 
+    const bookTitle = form.booktitle.value;
+    const author = form.author.value;
+    const bookContents = form.book_contents.value;
+
+    console.log(bookTitle, author, bookContents)
+    if(!bookTitle || !author || !bookContents){ 
         e.preventDefault();
         alert(`Type required fields`); //값이 없는 경우 오류메세지 alert
         return false;
