59f8f97c5490d0839a834aa0de363d684bfcd7cf
hannah-developer <hannahchoi.developer@gmail.com>
2020-08-19 16:32:08 +0900

    preventDefault()

--- public/js/create.js
+++ public/js/create.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('form')
 
-const bookTitle = form.booktitle.value;
-const author = form.author.value;
-const bookContents = form.book_contents.value;
+// const bookTitle = form.booktitle.value;
+// const author = form.author.value;
+// const bookContents = form.book_contents.value;
 
 // function returnFalse(){
 //     if(!bookTitle||!author||!bookTitle){
@@ -11,14 +11,16 @@ const bookContents = form.book_contents.value;
 //     }
 // }
 
-form.addEventListener('submit', ()=>{
-    if(bookTitle && author && bookContents){ //값이 모두 있는 경우에만 submit
-        alert('successfully posted!');
-        return true;        
-    } 
-    else {
+form.addEventListener('submit', (e)=>{
+    console.log(form.booktitle.value,form.author.value,form.book_contents.value)
+    if(form.booktitle.value=="" || form.author.value=="" || form.book_contents.value==""){ 
+        e.preventDefault();
         alert(`Type required fields`); //값이 없는 경우 오류메세지 alert
         return false;
+    } 
+    else {//값이 모두 있는 경우에만 submit
+        alert('successfully posted!');
+        return true;     
 }
 }
 );
--- routes/index.js
+++ routes/index.js
@@ -18,15 +18,5 @@ router.post('/create',function(req,res){
   });
 })
 
-// router.post('/create',function(req,res){
-//   if(req.body.author && req.body.booktitle && req.body.book_contents){
-//   connection.query(`insert into bookquotes(author,title,contents) values('${req.body.author}','${req.body.booktitle}','${req.body.book_contents}')`, function(err, rows, fields) {
-//     if (err) throw err;
-//     res.redirect('/')
-//   });}
-//   else { //db d
-//     res.redirect('/create')
-//   }
-// })
 
 module.exports = router;
