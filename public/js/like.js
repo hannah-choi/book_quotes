190a733ac4358abf27eada18f95b73780738210f
hannah-developer <hannahchoi.developer@gmail.com>
2021-03-25 15:02:47 +0000

    parameterized query & css update

--- public/js/like.js
+++ public/js/like.js
@@ -1,26 +1,27 @@
-
-$(document).ready(function(){
-    $('.fa-heart').on('click', function(e){
-        const postid = $(this).data('postid');
+$(document).ready(function () {
+    $(".fa-heart").on("click", function (e) {
+        const postid = $(this).data("postid");
         let like = e.target.dataset.like;
         $.ajax({
-          url:`/likeupdate`,
-          method:'get',
-          data:{
-            like:like,
-            id:postid, 
-          },
-          success: function(response){
-            if(like==1){
-                e.target.classList.remove('far')
-                e.target.classList.add('fas')
-                e.target.dataset.like = 0;
-            } else {
-                e.target.classList.add('far')
-                e.target.classList.remove('fas')
-                e.target.dataset.like = 1;
-            }
-          }
-    })
-    })
-    })
+            url: `/likeupdate`,
+            method: "get",
+            data: {
+                like: like,
+                id: postid,
+            },
+            success: function (response) {
+                if (response === "OK" && parseInt(like) === 1) {
+                    e.target.classList.remove("far");
+                    e.target.classList.add("fas");
+                    e.target.dataset.like = 0;
+                } else if (response === "OK" && parseInt(like) === 0) {
+                    e.target.classList.add("far");
+                    e.target.classList.remove("fas");
+                    e.target.dataset.like = 1;
+                } else {
+                    return;
+                }
+            },
+        });
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,95 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const connection = require('../bin/db.js');
-const { rollback } = require('../bin/db.js');
+const db = require("../bin/db.js");
+const { rollback } = require("../bin/db.js");
 
-router.get('/', function(req, res, next) {
-	connection.query(`SELECT id, author, title, contents, \`like\`, date_format(date, '%d %b, %Y') AS date from bookquotes ORDER BY id DESC;`, (err,rows,fields)=>{
-	  res.render('index', {rows:rows});
-	})
-  });
+router.get("/", function (req, res, next) {
+    const columns = ["id", "author", "title", "contents", "heart"];
+    db.query(
+        `SELECT ??, date_format(date, '%d %b, %Y') AS date from bookquotes ORDER BY id DESC;`,
+        [columns],
+        (err, rows, fields) => {
+            res.render("index", { rows: rows });
+        }
+    );
+});
 
-router.get('/create',function(req,res){
-  res.render('create',{ 
-		pageTitle: 'Write new post',
-        action: '/create',
-        title: '',
-        author: '',
-        contents: '',
-        id: ''
-    })
-})
+router.get("/create", function (req, res) {
+    res.render("create", {
+        pageTitle: "Write new post",
+        action: "/create",
+        title: "",
+        author: "",
+        contents: "",
+        id: "",
+    });
+});
 
-router.post('/create',function(req,res){
-  connection.query(`INSERT INTO bookquotes(author,title,contents) VALUES('${req.body.author}','${req.body.bookTitle}','${req.body.bookContents}')`, function(err, rows, fields) {
-    if (err) throw err;
-    res.redirect('/')
-      });  
-})
+router.post("/create", function (req, res) {
+    const columns = ["author", "title", "contents"];
+    const values = [req.body.author, req.body.bookTitle, req.body.bookContents];
+    db.query(
+        `INSERT INTO bookquotes(??) VALUES(?)`,
+        [columns, values],
+        function (err, rows, fields) {
+            if (err) throw err;
+            res.redirect("/");
+        }
+    );
+});
 
-router.get('/update',function(req,res){
-  connection.query(`SELECT * FROM bookquotes WHERE ID = ${req.query.id}`, (err,rows,fields)=>{
-	res.render('create',{ 
-		pageTitle: 'Update post',
-        action: '/update',
-        title: rows[0].title,
-        author: rows[0].author,
-        contents: rows[0].contents,
-		id: rows[0].id
-		})
-  })
-})
+router.get("/update", function (req, res) {
+    const param = { id: +req.query.id };
+    db.query(`SELECT * FROM bookquotes WHERE ?`, param, (err, rows, fields) => {
+        res.render("create", {
+            pageTitle: "Update post",
+            action: "/update",
+            title: rows[0].title,
+            author: rows[0].author,
+            contents: rows[0].contents,
+            id: rows[0].id,
+        });
+    });
+});
 
-router.post('/update', function(req,res){
-	connection.query(`UPDATE bookquotes SET author = '${req.body.author}', title = '${req.body.bookTitle}', contents= '${req.body.bookContents}' WHERE id=${req.body.id}`, function(err, rows, fields) {
-		if (err) throw err;
-		res.redirect('/')
-	})
-})
+router.post("/update", function (req, res) {
+    const param1 = { author: req.body.author };
+    const param2 = { title: req.body.bookTitle };
+    const param3 = { contents: req.body.bookContents };
+    const param4 = { id: +req.body.id };
+    db.query(
+        `UPDATE bookquotes SET ?, ?, ? WHERE ?`,
+        [param1, param2, param3, param4],
+        function (err, rows, fields) {
+            if (err) throw err;
+            res.redirect("/");
+        }
+    );
+});
 
-router.get('/likeupdate',function(req,res){
-	const like = req.query.like;
-	connection.query(`UPDATE bookquotes SET \`like\`= ${like} WHERE id =${req.query.id}`,(err,rows,fields)=>{
-    	if (err) throw err;
-    	res.sendStatus(200);
-  	})
-})
+router.get("/likeupdate", function (req, res) {
+    const param1 = { heart: +req.query.like };
+    const param2 = { id: +req.query.id };
+    db.query(
+        `UPDATE bookquotes SET ? WHERE ?`,
+        [param1, param2],
+        (err, rows, fields) => {
+            if (err) throw err;
+            res.sendStatus(200);
+        }
+    );
+});
 
-
-router.get('/delete',function(req,res){
-	connection.query(`DELETE FROM bookquotes WHERE id=${req.query.id}`, function(err, rows, fields) {
-		if (err) throw err;
-		res.redirect('/')
-  	})
-})
+router.get("/delete", function (req, res) {
+    const param = { id: req.query.id };
+    db.query(
+        `DELETE FROM bookquotes WHERE ?`,
+        param,
+        function (err, rows, fields) {
+            if (err) throw err;
+            res.redirect("/");
+        }
+    );
+});
 
 module.exports = router;
